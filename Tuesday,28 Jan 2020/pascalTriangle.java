338b9fc15d3fd21c95e6e55048fc598addf2c231
ankit986 <ab98647@gmail.com>
2020-02-04 16:16:25 +0530

    Added Solution

---
+++ Tuesday,28 Jan 2020/pascalTriangle.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> output= new ArrayList<List<Integer>>();//For C++ use vector instead of List
+       
+        for(int i=0;i<numRows;i++){
+                   
+        List<Integer> currentList = new ArrayList<>();
+            for(int j=0;j<i+1;j++){
+                if(j==0 || j== i){
+                    currentList.add(1);
+                }
+               
+                else{
+                    currentList.add(output.get(i-1).get(j-1)+output.get(i-1).get(j));                 
+		  }
+            }
+            output.add(currentList);
+       }
+        return output;
+    }
+}
